48ded79c350ba5d3eab5385a889e93d0fd57fc7b
jessburnett <jess.a.burnett@gmail.com>
2019-07-21 17:17:15 -0400

    contact context && global state

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -7,7 +7,9 @@ const About = () => {
       <div className="p bg-dark">
         <strong>Version:</strong> 1.0.0
         <p>A fullstack React App for Keeping Contacts</p>
-        <p><strong>Author: </strong>Jess Burnett</p>        
+        <p><strong>Author - Client Side: </strong>Jess Burnett</p>   
+        <p><strong>Author - API/Backend: </strong>Brad Traversy</p>   
+        <p><strong>Repo: </strong><a style={{color: 'grey'}} href="https://github.com/jessburnett/react-contact-fullstack" target="_blank">Public Github Repo</a></p>     
       </div>
     </div>
   )
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      Home Bitches
+      <h1>Home Bitches</h1>
     </div>
   )
 }
