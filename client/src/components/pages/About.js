aeadd00505bef9daf5784efd6bac46ecb5bf5870
jessburnett <jess.a.burnett@gmail.com>
2019-07-21 14:49:11 -0400

    client init cleanup && routing

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <div className="p bg-dark">
+        <strong>Version:</strong> 1.0.0
+        <p>A fullstack React App for Keeping Contacts</p>
+        <p><strong>Author: </strong>Jess Burnett</p>        
+      </div>
+    </div>
+  )
+}
+
+export default About
